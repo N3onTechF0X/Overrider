0ef925cedc89a49d6b966e75afd58c99153eab1c
N3onTechF0X <128133812+N3onTechF0X@users.noreply.github.com>
2025-03-01 11:53:12 +0200

    Update overrider.user.js

--- overrider.user.js
+++ overrider.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Overrider
-// @version      3.1.1
+// @version      3.2.0
 // @description  Override any file in game
 // @author       N3onTechF0X
 // @icon         https://raw.githubusercontent.com/N3onTechF0X/Overrider/main/logo.png
 // @match        https://*.tankionline.com/*
-// @require      https://raw.githubusercontent.com/N3onTechF0X/Overrider/main/consts.js
-// @require      https://raw.githubusercontent.com/N3onTechF0X/Overrider/main/overrider.src.js
-// @require      https://raw.githubusercontent.com/N3onTechF0X/Overrider/main/utils.js
+// @require      https://raw.githubusercontent.com/N3onTechF0X/Overrider/refs/heads/main/consts.js
+// @require      https://raw.githubusercontent.com/N3onTechF0X/Overrider/refs/heads/main/overrider.src.js
+// @require      https://raw.githubusercontent.com/N3onTechF0X/Overrider/refs/heads/main/utils.js
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
